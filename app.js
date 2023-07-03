7d7ea7d8ddf5dca04abf458ee28e13821b13da43
Mert Çetin <mrtctn506@gmail.com>
2023-07-03 13:00:56 +0300

    js dosyasina document dosyalari cagrildi

---
+++ app.js
@@ -0,0 +1,5 @@
+const textDOM = document.querySelector('.text');
+const addDOM = document.getElementById('btn-add')
+const clearDOM = document.getElementById('btn-clear');
+const listDOM = document.getElementById('list');
+let counterDOM = document.querySelector('.counter');
